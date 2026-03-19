449ef63d0aa45ae9a1a9c7a233408a98e729287e
ImArnavRana <arnavrana0704@gmail.com>
2026-03-19 23:55:28 +0530

    UC1: Add welcome message module

--- src/BOOKmystayapp.java
+++ src/BOOKmystayapp.java
@@ -1,15 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/**
+ * UseCase1HotelBookingApp
+ *
+ * This class represents the entry point of the Hotel Booking Management System.
+ * It demonstrates how a Java application starts execution and displays
+ * basic application information on the console.
+ *
+ * @author Arnav Rana
+ * @version 1.0
+ */
 public class BOOKmystayapp {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    /**
+     * Main method - Entry point of the application
+     * JVM starts execution from here
+     */
+    public static void main(String[] args) {
+
+        // Welcome Message
+        System.out.println("=======================================");
+        System.out.println("      BOOK MY STAY APPLICATION");
+        System.out.println("=======================================");
+
+        // Application Details
+        System.out.println("Application: Hotel Booking Management System");
+        System.out.println("Version: 1.0");
+
+        System.out.println("---------------------------------------");
+        System.out.println("Welcome! Your journey to easy hotel booking starts here.");
+        System.out.println("---------------------------------------");
+
+        // End of UC1
+        System.out.println("Application started successfully.");
     }
 }
